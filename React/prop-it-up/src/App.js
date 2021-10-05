9b76e9707947e09f3f3f157073bfb7ec0fc996fd
Kishan Patel <kishapat@cisco.com>
2021-10-05 15:36:03 -0700

    added prop-it-up files

---
+++ React/prop-it-up/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+const App = () => {
+
+  const personObject = [
+    {
+      firstName:"Jane",
+      lastName:"Doe",
+      age: 45,
+      hairColor:"Black"
+    },
+
+    {
+      firstName:"John",
+      lastName:"Smith",
+      age: 88,
+      hairColor:"Brown"
+    },
+
+    {
+      firstName:"Millard",
+      lastName:"Fillmore",
+      age: 50,
+      hairColor:"Brown"
+    },
+
+    {
+      firstName:"Maria",
+      lastName:"Smith",
+      age: 62,
+      hairColor:"Brown"
+    }
+  ];
+
+  return (
+    <div className="App">
+      {
+        personObject.map((object, index) => (
+          <PersonCard key={index}
+            firstName={object.firstName}
+            lastName={object.lastName}
+            age={object.age}
+            hairColor={object.hairColor}
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
