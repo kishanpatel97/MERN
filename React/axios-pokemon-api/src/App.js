9629e1a92e9fcd906b27b49e684142ca066a2aa9
Kishan Patel <kishapat@cisco.com>
2021-10-20 14:58:42 -0700

    axios api practice

--- React/axios-pokemon-api/src/App.js
+++ React/axios-pokemon-api/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then((response) => {
+                setPokemon(response.data.results)
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    }, []);
+
+    return (
+        <div>
+            <ul>
+                {pokemon.map((poke, index) => {
+                    return <li key={index}>{poke.name}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
